3114cbfc38a7545f7f7f19ae4350ddf24e32ef0c
‘greatjinga’ <alexsmolov17@gmail.com>
2022-12-13 17:56:32 +0300

    iniitial

---
+++ WhiteAppsAlekseev/WhiteAppsAlekseev/WhiteAppsAlekseevApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhiteAppsAlekseevApp.swift
+//  WhiteAppsAlekseev
+//
+//  Created by Руслан Алексеев on 08.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct WhiteAppsAlekseevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
